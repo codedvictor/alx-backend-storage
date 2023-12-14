37a50e6d50ee50078ed6e99be0c85e00e25af747
Ige Victor Kolade <victorkoladeige@gmail.com>
2023-12-14 17:15:22 +0000

    in and out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- create tabel users if not exist
+-- table attribute id, email, name
+CREATE TABLE IF NOT EXISTS users (
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
+
