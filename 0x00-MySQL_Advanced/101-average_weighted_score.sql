b6a47b1a179e62cc1d9a3046a23db97710271fe3
Ige Victor Kolade <victorkoladeige@gmail.com>
2023-12-14 21:46:22 +0000

    average weight

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,18 @@
+-- creates a stored procedure ComputeAverageWeightedScoreForUsers
+-- that computes and store the average weighted score for all students
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+    UPDATE users AS indvd, 
+        (SELECT indvd.id, SUM(score * weight) / SUM(weight) AS w_avg 
+        FROM users AS indvd 
+        JOIN corrections as C ON indvd.id=C.user_id 
+        JOIN projects AS P ON C.project_id=P.id 
+        GROUP BY indvd.id)
+    AS WA
+    SET U.average_score = WA.w_avg 
+    WHERE indvd.id=WA.id;
+END
+$$
+DELIMITER ;
