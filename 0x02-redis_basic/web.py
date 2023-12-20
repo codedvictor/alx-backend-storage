b8bf1322e6a2d6577faefa1f125c6afedaa2c770
Ige Victor Kolade <victorkoladeige@gmail.com>
2023-12-20 16:43:40 +0000

    rewrite code

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,30 +9,24 @@ from typing import Callable
 
 
 redis_store = redis.Redis()
-"""
-redis instance module
-"""
 
 
 def data_cacher(method: Callable) -> Callable:
     """
     Caches the output of fetched data.
     """
-    @wraps(method)
     def invoker(url) -> str:
-        """
-        The wrapper function for caching the output.
-        """
+        '''The wrapper function for caching the output.
+        '''
         redis_store.incr(f'count:{url}')
-        get_result = redis_store.get(f'result:{url}')
-        if get_result:
-            return get_result.decode('utf-8')
-        get_result = method(url)
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
         redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, get_result)
-        return get_result
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
     return invoker
-
 
 @data_cacher
 def get_page(url: str) -> str:
